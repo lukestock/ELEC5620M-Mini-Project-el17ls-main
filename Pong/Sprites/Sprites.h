a4f8c75a6e009b8117b849fd3044566f55ee617d
lukestock <113342189+lukestock@users.noreply.github.com>
2022-09-12 14:11:59 +0100

    Add files via upload

---
+++ Pong/Sprites/Sprites.h
@@ -0,0 +1,16 @@
+/*
+ * Sprites.h
+ *
+ *  Created on: 16 May 2022
+ *      Author: lukes
+ */
+
+#ifndef SPRITES_H_
+#define SPRITES_H_
+
+extern const unsigned short paddleSprite[1024];
+extern const unsigned short ballSprite[324];
+extern const unsigned short background[76800];
+extern const unsigned short intro_background[76800];
+
+#endif /* SPRITES_H_ */
